0b7d541493671e533b333c7c56e3d10ee7fbed4f
jasper david <jhasdavid@yahoo.com>
2012-08-27 16:51:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = 'a2fbca35db91d07ad8949e7c743ad5834b8de9cbc76ab229cfe70a01eaf296872dc8f5ba1263df4f83b44cd16e1efbfb28aa054354fecd5e837326faa096c10b'
---
+++ public/ruby.html.rb
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<style type="text/css">
+
+</style>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Ruby</title>
+</head>
+
+<body background="image/bg2.png">
+<br />
+<br />
+<br />
+<center><a href="prayer.html"><img src="image/ruby1.png" height="600" class="r" /></a><center />
+</body>
+</html>
